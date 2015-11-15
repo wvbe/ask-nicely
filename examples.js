cc975248cfc7756bcfb64a98a6ad514361fc1fde
wvbe <wybe@x-54.com>
2015-11-16 00:39:57 +0100

    Example help controller includes ancestry options & parameters + frmatting changes here

--- examples.js
+++ examples.js
@@ -1,6 +1,7 @@
 module.exports = {
 	controller: {
-		// uses console.log and some basic formatting
+		// uses console.log and some basic formatting to output "help" information on what is known about the
+		// controlling command. this is an example of how help pages could be generated.
 		help: require('./examples/controller.help')
 	}
 };
--- examples/controller.help.js
+++ examples/controller.help.js
@@ -1,45 +1,48 @@
 'use strict';
 
+const NO_NAME = '<no name>',
+	NO_DESCRIPTION = '<no description>',
+	NONE = '<none>';
+
+function concatAllOfAncestors (command, propertyName) {
+	return (command.parent
+		? concatAllOfAncestors(command.parent, propertyName)
+		: []).concat(command[propertyName]);
+}
+
 module.exports = function helpController(req) {
 	let command = req.command;
 
 	console.log(``);
-	console.log(`Name:        ${command.name || '(no name)'}`);
-	console.log(`Description: ${command.description || '(no description)'}`);
+	console.log(`Name:            ${command.name || NO_NAME}`);
+	console.log(`Description:     ${command.description || NO_DESCRIPTION}`);
 
-	if(command.children.length) {
-		console.log(`\n${command.children.length} Child commands:`);
+	console.log(`Child commands:  ${command.children.length || NONE}`);
+	command.children
+		.sort((a, b) => a.name < b.name ? -1 : 1)
+		.forEach(cmd =>{
+			console.log(`    ${cmd.name}    ${cmd.description || NO_DESCRIPTION}`);
+		});
 
-		command.children
-			.sort((a, b) => a.name < b.name ? -1 : 1)
-			.forEach(cmd =>{
-				console.log(`    ${cmd.name}: (${cmd.description || 'no description'})`);
+	let parameters = concatAllOfAncestors(command, 'parameters');
+	console.log(`Parameters:      ${parameters.length || NONE}`);
+	parameters
+		.forEach(param => {
+			console.log(`    {${param.name}}    ${param.description || NO_DESCRIPTION}`);
+		});
+
+	let options = concatAllOfAncestors(command, 'options');
+	console.log(`Options:         ${options.length || NONE}`);
+	options
+		.sort((a, b) => a.name < b.name ? -1 : 1)
+		.forEach(option => {
+			console.log([
+				'',
+				option.short ? '-' + option.short : '  ',
+				'--' + option.name,
+				option.required ? '* ' : '' + (option.description || NO_DESCRIPTION)
+			].join('    '));
 			});
-	}
-
-	if(command.parameters.length) {
-		console.log(`\n${command.parameters.length} Parameters:`);
-
-		command.parameters
-			.forEach(param => {
-				console.log(`    ${param.name}: (${param.description || 'no description'})`);
-			});
-	}
-
-	if(command.options.length) {
-		console.log(`\n${command.options.length} Parameters:`);
-
-		command.options
-			.sort((a, b) => a.name < b.name ? -1 : 1)
-			.forEach(option => {
-				console.log([
-					'',
-					(option.short ? '-' + option.short : '  '),
-					'--' + option.name,
-					(option.required ? '* ' : '') + (option.description || '(no description)')
-				].join('    '));
-			});
-	}
 
 	console.log(``);
 
