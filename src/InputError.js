6a7c0070c3d12b3c87a53cafcd6d57c3b5604c6a
Marijn van Butselaar <marijn@vanbutselaar.nl>
2018-08-14 19:03:41 +0200

    Fix instanceof InputError not working due to babel transform

--- src/InputError.js
+++ src/InputError.js
@@ -6,5 +6,9 @@ export default class AskNicelyInputError extends Error {
 
 		this.name = this.constructor.name;
 		this.solution = solution;
+
+		// A workaround to make `instanceof AskNicelyInputError` work in ES5 after babel transform
+		this.constructor = AskNicelyInputError;
+		this.__proto__ = AskNicelyInputError.prototype;
 	}
 }
